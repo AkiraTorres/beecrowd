6121368dc5e556750eeffdfdafe8d9f29f66a05f
Thiago Torres <torres.thg@gmail.com>
2022-03-02 21:10:31 -0300

    1004 - Produto Simples

---
+++ Respostas/Python/1004.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+x = int(input())
+y = int(input())
+
+prod = x * y
+
+print(f"PROD = {prod}")
