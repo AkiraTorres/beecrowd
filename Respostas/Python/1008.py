6592d8ed84086f09ae72e6308c53d876b0d69758
Thiago Torres <torres.thg@gmail.com>
2022-03-02 21:51:49 -0300

    ADD: 1008 - Salário

---
+++ Respostas/Python/1008.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+func_num = int(input())
+horas_trab = int(input())
+valor_hora = float(input())
+
+salario = horas_trab * valor_hora
+
+print(f"NUMBER = {func_num}")
+print(f"SALARY = U$ {salario:.2f}")
