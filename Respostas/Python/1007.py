ae1d376321019c4e2ca2bd227b2749c8574111a7
Thiago Torres <torres.thg@gmail.com>
2022-03-02 21:43:19 -0300

    ADD: 1007 - Diferença

---
+++ Respostas/Python/1007.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+diferenca = (a * b) - (c * d)
+
+print(f"DIFERENCA = {diferenca}")
