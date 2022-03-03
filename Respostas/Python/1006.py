617753d7a6191acbee0e77cb6117fd8de5332f37
Thiago Torres <torres.thg@gmail.com>
2022-03-02 21:11:09 -0300

    1006 - Média 2

---
+++ Respostas/Python/1006.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+# a, b, c = [float(nota) for nota in input().split(' ')]
+a = float(input())
+b = float(input())
+c = float(input())
+
+media = ((a * 2) + (b * 3) + (c * 5)) / 10
+
+print(f"MEDIA = {media:.1f}")
