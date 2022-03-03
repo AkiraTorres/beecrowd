6ea541c97182ad62318466f7c4d24f2be8861d86
Thiago Torres <torres.thg@gmail.com>
2022-03-03 00:40:11 -0300

    ADD: 1014 - Consumo

---
+++ Respostas/Python/1014.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+distancia_total = int(input())
+combustivel_gasto = float(input())
+
+consumo = distancia_total / combustivel_gasto
+
+print(f"{consumo:.3f} km/l")
