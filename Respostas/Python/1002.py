069193d6ec9cef63e19e98a766ba7e2178f474d6
Thiago Torres <torres.thg@gmail.com>
2022-03-02 21:09:12 -0300

    1002 - Área do Círculo

---
+++ Respostas/Python/1002.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+n = 3.14159
+raio = float(input())
+
+area = n * (raio ** 2)
+
+print(f"A={area:.4f}")
