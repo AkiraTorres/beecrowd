5db2f0b0df5c2e9099d60f0cee9edb37c38efc6e
Thiago Torres <torres.thg@gmail.com>
2022-03-03 00:39:34 -0300

    ADD: 1011 - Esfera

---
+++ Respostas/Python/1011.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+pi = 3.14159
+raio = float(input())
+
+volume = (4.0/3) * pi * (raio ** 3)
+
+print(f"VOLUME = {volume:.3f}")
