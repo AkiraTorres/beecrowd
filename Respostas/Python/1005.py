5028951cc1237580763dd21fe9fd1d8de0903606
Thiago Torres <torres.thg@gmail.com>
2022-03-02 21:10:55 -0300

    1005 - Média 1

---
+++ Respostas/Python/1005.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+a = float(input())
+b = float(input())
+
+media = ((a * 3.5) + (b * 7.5)) / 11
+
+print(f"MEDIA = {media:.5f}")
