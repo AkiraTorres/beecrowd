31004bf314bf05503597f2b69f0416317a99edc3
Thiago Torres <torres.thg@gmail.com>
2022-03-02 21:08:35 -0300

    1001 - Extremamente Básico

---
+++ Respostas/PHP/1001.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Escreva a sua solução aqui
+ * Code your solution here
+ * Escriba su solución aquí
+ */
+
+$a = $argc[0];
+$b = $argc[1];
+
+$x = $a + $b;
+
+echo "X = " . $x . PHP_EOL;
+
+?>
---
+++ Respostas/Python/1001.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+'''
+Escreva a sua solução aqui
+Code your solution here
+Escriba su solución aquí
+'''
+
+a, b = [int(x) for x in input().split(' ')]
+x = a + b
+
+print(f'X = {x}\n');
